e597ea6868973910c43e45675eb5b0c9b38520b2
Margherita Groaz <margherita.groaz@mail.polimi.it>
2023-12-01 14:16:41 +0100

    Gioco memory completato

--- Giuoco/Assets/Scenes/GiocoVirgi/MemoryManager.cs
+++ Giuoco/Assets/Scenes/GiocoVirgi/MemoryManager.cs
@@ -12,24 +12,25 @@ public class MemoryManager : MonoBehaviour
     public List<Button> btns = new List<Button>();
     private bool firstGuess, secondGuess;
     private int countGuesses;
-    private int cointCorrectGuesses;
-    private int gameGuesses;
+    private int countCorrectGuesses;
     private int firstGuessIndex, secondGuessIndex;
     private string firstGuessPuzzle, secondGuessPuzzle;
+    private int gameGuesses; // Aggiunto il contatore delle partite
+
     private void Awake()
     {
-        puzzles = Resources. LoadAll<Sprite>("fruit");
+        puzzles = Resources.LoadAll<Sprite>("fruit");
     }
 
-    // Start is called before the first frame update
     void Start()
     {
         GetButtons();
         AddListeners();
         AddGamePuzzles();
+        Shuffle(gamePuzzles);
+        gameGuesses = gamePuzzles.Count / 2;
     }
 
-    // Update is called once per frame
     void GetButtons()
     {
         GameObject[] objects = GameObject.FindGameObjectsWithTag("puzzleBtn");
@@ -39,31 +40,106 @@ public class MemoryManager : MonoBehaviour
             btns[i].image.sprite = bgImage;
         }
     }
+
     void AddGamePuzzles()
     {
         int looper = btns.Count;
         int index = 0;
-        for (int i = 0; 1 < looper; i++)
+        for (int i = 0; i < looper; i++)
         {
-            if(index == looper/2)
+            if (index == looper / 2)
             {
                 index = 0;
             }
-                gamePuzzles. Add (puzzles[index]);
-                index++;
+            gamePuzzles.Add(puzzles[index]);
+            index++;
         }
     }
+
     private void AddListeners()
     {
         foreach (Button btn in btns)
         {
-            btn.onClick.AddListener(() => PickPuzzle()); // Fix: Corretto il nome della funzione
+            btn.onClick.AddListener(() => PickPuzzle());
         }
     }
 
-    private void PickPuzzle() // Fix: Aggiunto il corpo della funzione
+    private void PickPuzzle()
     {
-        string name = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name;
-        print("Hey " + name);
+        if (!firstGuess)
+        {
+            firstGuess = true;
+            firstGuessIndex = int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
+
+            firstGuessPuzzle = gamePuzzles[firstGuessIndex].name;
+
+            btns[firstGuessIndex].image.sprite = gamePuzzles[firstGuessIndex];
+        }
+        else if (!secondGuess)
+        {
+            secondGuess = true;
+            secondGuessIndex = int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
+
+            secondGuessPuzzle = gamePuzzles[secondGuessIndex].name;
+
+            btns[secondGuessIndex].image.sprite = gamePuzzles[secondGuessIndex];
+            if (firstGuessPuzzle == secondGuessPuzzle)
+            {
+                print("Puzzle Match");
+                StartCoroutine(CheckThePuzzleMatch());
+            }
+            else
+            {
+                print("Puzzle don't Match");
+                StartCoroutine(CheckThePuzzleMatch());
+            }
+        }
+    }
+
+    IEnumerator CheckThePuzzleMatch()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        if (firstGuessPuzzle == secondGuessPuzzle)
+        {
+            btns[firstGuessIndex].interactable = false;
+            btns[secondGuessIndex].interactable = false;
+            btns[firstGuessIndex].image.color = new Color(0, 0, 0, 0);
+            btns[secondGuessIndex].image.color = new Color(0, 0, 0, 0);
+            countCorrectGuesses++;
+
+            CheckTheGameFinished();
+        }
+        else
+        {
+            btns[firstGuessIndex].image.sprite = bgImage;
+            btns[secondGuessIndex].image.sprite = bgImage;
+        }
+
+        yield return new WaitForSeconds(0.5f);
+
+        firstGuess = secondGuess = false;
+    }
+
+    void CheckTheGameFinished()
+    {
+        countGuesses++;
+
+        if (countCorrectGuesses == gameGuesses)
+        {
+            print("End of the game");
+            print("It took you " + countGuesses + " guesses.");
+        }
+    }
+
+    void Shuffle(List<Sprite> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            Sprite temp = list[i];
+            int randomIndex = Random.Range(i, list.Count);
+            list[i] = list[randomIndex];
+            list[randomIndex] = temp;
+        }
     }
 }
